974cf93f24997f25882e6a5e24a5369429391112
Wendy Beck <webeck@gmail.com>
2017-08-15 22:10:56 -0700

    nothing is broken but nothing is working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './index.css';
 
+const prompts= [
+      [0, 'Choose a writing prompt', ''],
+      [1, 'sudden riches', ''],
+      [2,  'sudden fame',  ''],
+      [3,  'tragedy',  ''],
+      [4,  'love',  ''],
+      [5,  'school',  ''],
+      [6,  'travel',  ''],
+      [7 , 'moving to a new city',  ''],
+      [8,  'apocalypse',  ''],
+      [9,  'adventure',  ''],
+      [10 , 'childhood',  '']
+];
+
 
 class App extends Component {
 
@@ -16,85 +30,67 @@ class App extends Component {
       name: 'wrld',
       charCount: 0,
       value: '',
-      editor: '',
-      prompts: [
-      ['Choose a writing prompt', ''],
-      ['sudden riches', ''],
-      [  'sudden fame',  ''],
-      [  'tragedy',  ''],
-      [  'love',  ''],
-      [  'school',  ''],
-      [  'travel',  ''],
-      [  'moving to a new city',  ''],
-      [  'apocalypse',  ''],
-      [  'adventure',  ''],
-      [  'childhood',  '']]
-  };
+      editor: ''
+      }
+    }
 
- this.promptItems = this.state.prompts.map(prompt => {
-      return <option name="promptOption" value="{prompt[1]}">{prompt[0]}</option>
-})
+    // console.log(SelectPrompt());
+    handleClick({name, value}) {
+      console.log('click: ',name,value);
+    }
 
+    handleChange({name, value}) {
+      console.log('change: ',name,value);
+      console.log(typeof prompts[0][0]);
+      // this.setState({ [name]: value });
+      // this.setState[target.value] = target.value;
+    }
 
+    handlePromptSelect(target) {
+      console.log('select: ', target);
+    }
 
-}
-
-
-
-
-
-
-
-  handleClick({name, value}) {
-    console.log('click: ',name,value);
-  }
-
-
-  handleChange({name, value}) {
-    console.log('change: ',name,value);
-    // this.setState({ [name]: value });
-    // this.setState[target.value] = target.value;
-  }
-
-  handleSelect(target) {
-    console.log('select: ', target);
-  }
-
-  render() {
-    return (
-      <div className="App wrapper">
-      <div className="App-logo"/>
-      <h3> eddy - itor awaits your input </h3>
-      <div>
-      <label>
-      Clicky
-      <select name="promptSelect">
-      {this.promptItems}
-      </select>
-
-
-      </label>
-
-<div>
+   Prompts({ prompts }) {
+      return (
+        <select value={this.state.value} onChange={this.handleChange}>
+        {prompts.map(prompt => (
+            <option key={prompt[0]} prompt={prompt[1]}/>
+          ))}
+        </select>
+      );
+    }
 
 
 
 
-      <label>
-      Enter text here:
-        <input name = "editor" value = {this.state.editor} onChange = {({target}) =>this.handleChange(target)}/>
-      </label>
-      </div>
-      <div>
+
+
+    render() {
+      return (
+        <div className="App wrapper">
+        <div className="App-logo"/>
+        <h3> eddy - itor awaits your input </h3>
+        <div>
+        <Prompt/>
+        </div>
+
+        <div>
+
+        <label>
+        Enter text here:
+        <textarea name ="editor" value = {this.state.editor} onChange = {({target}) =>this.handleChange(target)}/>
+        </label>
+        </div>
+        <div>
         <label>
         characters:
         <div name="charCount" value={this.state.charCount}>{this.state.charCount}</div>
         </label>
         </div>
-    </div>
-    </div>
-    );
-  }
-}
+        </div>
 
-export default App;
+      );
+    }
+  }
+
+  export default App;
